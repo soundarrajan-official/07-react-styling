0d23d847284549d7152b86e1114f28b1990c08ca
soundarrajan-official <soundarrajan.official@gmail.com>
2024-05-13 14:10:32 +0530

    Add inline styling for paragraph tag in Header Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,11 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{
+          color: "green",
+          textAlign: "left",
+          margin: 0
+      }}>A community of artists and art-lovers.</p>
     </header>
   );
 }
