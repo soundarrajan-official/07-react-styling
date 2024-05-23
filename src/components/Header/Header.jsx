6cbb0fa47d4ce04be1fdffde7af29abce76cb453
soundarrajan-official <soundarrajan.official@gmail.com>
2024-05-23 10:40:22 +0530

    Adding and using the tailwindcss in react app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="flex flex-col items-center mt-8 mb-16">
       <img className="mb-8 w-44 h-44" src={logo} alt="A canvas" />
       <h1 className="text-4xl font-semibold tracking-wider text-center uppercase text-amber-800 font-title">ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
