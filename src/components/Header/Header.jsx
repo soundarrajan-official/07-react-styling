8bfd15a21eb8b35b6f5547cb6b7c988a3a3de10b
soundarrajan-official <soundarrajan.official@gmail.com>
2024-05-25 11:05:01 +0530

    Use tailwind media queries and pseudo selectors instead of styled component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import logo from '../../assets/logo.png';
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
       <img className="mb-8 w-44 h-44" src={logo} alt="A canvas" />
-      <h1 className="text-4xl font-semibold tracking-wider text-center uppercase text-amber-800 font-title">ReactArt</h1>
+      <h1 className="text-xl md:text-4xl font-semibold tracking-wider text-center uppercase text-amber-800 font-title">ReactArt</h1>
       <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
